3f4c68cdcb7d422b1a7f806f5336fb96a9382f05
Odaine <odaine.forbes@yahoo.com>
2017-04-29 05:25:23 +0000

    still no luck with thumbnail URL

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,10 @@ import os
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:project2@localhost/projectdb"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://qvufzfhjnrzrne:PWKGvd6x1FghXkr90I4MXo3kUG@ec2-54-225-112-119.compute-1.amazonaws.com:5432/d4fgu381smcp9t"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ppbtfcrjasdzzf:c46ece106f6383251e05184dd6993b541f32098b49feea373767e8724905a249@ec2-54-225-182-108.compute-1.amazonaws.com:5432/d36pnsfrqlt0in'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 db = SQLAlchemy(app)
 db.create_all()
 
 from app import views,models
+
